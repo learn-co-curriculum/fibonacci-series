3b283be8e8b54da4c9dd53034db013c04bc2ac6a
jackpbauer-work <124083011+jackpbauer-work@users.noreply.github.com>
2023-06-15 13:57:51 -0400

    fibonacci-series

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
-  // type your code here
+  if (num <= 1) {
+    return num;
+  } else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+
 }
 
 if (require.main === module) {
@@ -16,6 +21,23 @@ if (require.main === module) {
 
   console.log("Expecting: 55");
   console.log("=>", fibonacci(10));
+
+  console.log("");
+
+  console.log("Expecting: 6765");
+  console.log("=>", fibonacci(20));
+
+  console.log("");
+
+  console.log("Expecting: 832040");
+  console.log("=>", fibonacci(30));
+
+  console.log("");
+
+  console.log("Expecting: 102334155");
+  console.log("=>", fibonacci(40));
+
+  console.log("");
 }
 
 module.exports = fibonacci;
